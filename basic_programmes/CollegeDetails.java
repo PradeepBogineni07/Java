a33bda5ee13eca0d166e451ded5cd27b4bf70953
B Pradeep <pradeepbogineni9@gmail.com>
2025-04-28 11:38:00 +0530

    first

---
+++ basic_programmes/CollegeDetails.java
@@ -0,0 +1,7 @@
+package basic_programmes;
+
+public class CollegeDetails {
+	public static void main(String[]args){
+		System.out.println(" My name is : pradeep\n Id is : 19P81A0512\n College name: Vignana Bharathi Engineering College\n My Course : Btech\n Stream: Cse ");
+		}}
+
---
+++ basic_programmes/CollegeDetailsCommandLine.java
@@ -0,0 +1,12 @@
+package basic_programmes;
+
+public class CollegeDetailsCommandLine {
+	public static void main(String[] args) {
+        System.out.println("College Name: " + args[0]);
+        System.out.println("Location: " + args[1]);
+        System.out.println("Established Year: " + args[2]);
+        System.out.println("Number of Departments: " + args[3]);
+    }
+}
+
+
